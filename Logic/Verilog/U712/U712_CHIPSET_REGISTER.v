f7adeef0a675b2117df72bc721ace0204c64957b
Jason Neus <78610265+jasonsbeer@users.noreply.github.com>
2024-06-25 21:41:21 -0500

    Cleanup up chip register cycle timings. 25-JUN-2024

--- Logic/Verilog/U712/U712_CHIPSET_REGISTER.v
+++ Logic/Verilog/U712/U712_CHIPSET_REGISTER.v
@@ -26,6 +26,7 @@ Description: CHIP REGISTER (MC68000) CYCLES
 
 Revision History:
     12-JUN-2024 : Initial Code
+    25-JUN-2024 : Clean Up End Of Cycle Timing
 
 GitHub: https://github.com/jasonsbeer/AmigaPCI
 TO BUILD WITH APIO: apio build --top-module U712_TOP --fpga iCE40-HX4K-TQ144
@@ -62,11 +63,13 @@ reg REGTA_EN;
 reg [1:0] CLKC1;
 reg [1:0] CLKC3;
 reg REG_CYCLE_OUT;
+reg LDS_OUT;
+reg UDS_OUT;
 
 assign nREGEN = ~REG_EN;
 assign nAS = ~AS_EN;
-assign nLDS = ~((SIZ1 || !SIZ0 || A[0]) && DS_EN);
-assign nUDS = ~(!A[0] && DS_EN);
+assign nLDS = ~(LDS_OUT && DS_EN);
+assign nUDS = ~(UDS_OUT && DS_EN);
 assign REG_TA = REGTA_EN;
 assign REG_CYCLE = REG_CYCLE_OUT;
 
@@ -81,19 +84,27 @@ always @(negedge CLK40, negedge nRESET) begin
         REGTA_EN <= 0;
         STATE_COUNT <= 2'b00;  
         REG_CYCLE_OUT <= 0;
+        LDS_OUT <= 0;
+        UDS_OUT <= 0;
 
     end else begin
 
         case (STATE_COUNT)
 
             3'b00: //STATE 2
-                if (CLKC1 == 2'b00 && CLKC3 == 2'b10 && nREGSPACE == 1'b0) begin 
-                    AS_EN <= 1;
-                    REG_EN <= 1;                  
-                    if (RnW == 1) begin
-                        DS_EN <= 1;
-                    end        
-                    STATE_COUNT <= 2'b01;            
+                begin
+                    REGTA_EN <= 1'b0;
+
+                    if (CLKC1 == 2'b00 && CLKC3 == 2'b10 && nREGSPACE == 1'b0) begin 
+                        AS_EN <= 1;
+                        REG_EN <= 1;     
+                        LDS_OUT <= SIZ1 || !SIZ0 || A[0];
+                        UDS_OUT <= !A[0];
+                        if (RnW == 1) begin
+                            DS_EN <= 1;
+                        end        
+                        STATE_COUNT <= 2'b01;            
+                    end
                 end
             
             2'b01: //STATE 4
@@ -106,19 +117,24 @@ always @(negedge CLK40, negedge nRESET) begin
                 end
 
             2'b10: //STATE 6
-                if (CLKC1 == 2'b01 && CLKC3 == 2'b00) begin 
-                    REGTA_EN <= 1;                    
+                if (CLKC1 == 2'b00 && CLKC3 == 2'b10) begin 
+                    REGTA_EN <= RnW;                    
                     STATE_COUNT <= 2'b11; 
                 end
 
             2'b11 : //STATE 7
                 begin  
-                    REGTA_EN <= 1'b0;
-                    AS_EN <= 0;
-                    DS_EN <= 0;
-                    REG_EN <= 0;
-                    REG_CYCLE_OUT <= 0;
-                    if (CLKC1 == 2'b11 && CLKC3 == 2'b01) begin STATE_COUNT <= 2'b00; end
+                    if (RnW) begin REGTA_EN <= 1'b0; end
+
+                    if (CLKC1 == 2'b01 && CLKC3 == 2'b00) begin
+                        AS_EN <= 0;
+                        DS_EN <= 0;
+                        REG_EN <= 0;
+                        REG_CYCLE_OUT <= 0;
+                        STATE_COUNT <= 2'b00;
+                        if (!RnW) begin REGTA_EN <= 1'b1; end
+                    end
+
                 end
         endcase
     end
