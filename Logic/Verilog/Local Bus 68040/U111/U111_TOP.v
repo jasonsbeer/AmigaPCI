d06f1ebbb8631332a8dff5274da6710efbb9345e
Jason Neus <78610265+jasonsbeer@users.noreply.github.com>
2024-08-09 13:54:37 -0500

    Update U111_TOP.v

--- Logic/Verilog/Local Bus 68040/U111/U111_TOP.v
+++ Logic/Verilog/Local Bus 68040/U111/U111_TOP.v
@@ -42,14 +42,14 @@ input nTS_CPU, nTBI, nTCI, RnW, nBG, nRESET, CLK40,
 
 output nTS, nTA, nTBI_CPU, nTCI_CPU, CLK40A, CLK40B, CLK40C, CLK80A, CLK80B, CLK80C, RAMCLK,
 
-inout [31:24] D3V3A_BYTE0,
-inout [23:16] D3V3A_BYTE1,
-inout [15:8] D3V3A_BYTE2,
-inout [7:0] D3V3A_BYTE3,
-inout [31:24] D3V3B_BYTE0,
-inout [23:16] D3V3B_BYTE1,
-inout [15:8] D3V3B_BYTE2,
-inout [7:0] D3V3B_BYTE3
+inout [31:24] DA_BYTE0,
+inout [23:16] DA_BYTE1,
+inout [15:8] DA_BYTE2,
+inout [7:0] DA_BYTE3,
+inout [31:24] DB_BYTE0,
+inout [23:16] DB_BYTE1,
+inout [15:8] DB_BYTE2,
+inout [7:0] DB_BYTE3
 
 );
 
@@ -105,13 +105,6 @@ SB_PLL40_PAD # (
         .BYPASS(1'b0)
     );
 
-//THIS IS AN IMPLEMENTATION OF THE DYNAMIC BUS SIZING STATE MACHINE PRESENTED IN
-//THE "MC68040 DESIGNER'S HANDBOOK". SUPPORT FOR BYTE PORT TERMINATION IS NOT INCLUDED
-//BECAUSE THE AMIGAPCI DOES NOT NEED THIS. BOTH WORD AND LONG WORD TERMINATED CYCLES
-//ARE SUPPORTED.
-
-//BYTE0 IS THE MOST SIGNIFICANT. BYTE3 IS THE LEAST.
-
 /////////////////////////
 // CYCLE START AND END //
 /////////////////////////
@@ -142,32 +135,41 @@ always @(negedge BCLK, negedge nRESET) begin
     end
 end
 
+//////////////////////////////////////////////////////////////////////////////////////////
+// THIS IS AN IMPLEMENTATION OF THE DYNAMIC BUS SIZING STATE MACHINE PRESENTED IN       //
+// THE "MC68040 DESIGNER'S HANDBOOK". SUPPORT FOR BYTE PORT TERMINATION IS NOT INCLUDED //
+// BECAUSE THE AMIGAPCI DOES NOT NEED THIS. BOTH WORD AND LONG WORD TERMINATED CYCLES   //
+// ARE SUPPORTED.                                                                       //
+//////////////////////////////////////////////////////////////////////////////////////////
+
+//BYTE0 IS THE MOST SIGNIFICANT. BYTE3 IS THE LEAST.
+
 ////////////////////////
 // DRIVE THE DATA BUS //
 ////////////////////////
 
-reg [7:0]D3V3A_BYTE0_O;
-reg [7:0]D3V3A_BYTE1_O;
-reg [7:0]D3V3A_BYTE2_O;
-reg [7:0]D3V3A_BYTE3_O;
-wire [7:0]D3V3B_BYTE0_O;
-wire [7:0]D3V3B_BYTE1_O;
-wire [7:0]D3V3B_BYTE2_O;
-wire [7:0]D3V3B_BYTE3_O;
+reg [7:0]DA_BYTE0_O;
+reg [7:0]DA_BYTE1_O;
+reg [7:0]DA_BYTE2_O;
+reg [7:0]DA_BYTE3_O;
+wire [7:0]DB_BYTE0_O;
+wire [7:0]DB_BYTE1_O;
+wire [7:0]DB_BYTE2_O;
+wire [7:0]DB_BYTE3_O;
 
-wire D3V3AEN = RnW && !nBG && nRESET;
+wire DAEN = RnW && !nBG && nRESET;
 
-assign D3V3A_BYTE0 = D3V3AEN ? D3V3A_BYTE0_O : 8'bzzzzzzzz;
-assign D3V3A_BYTE1 = D3V3AEN ? D3V3A_BYTE1_O : 8'bzzzzzzzz;
-assign D3V3A_BYTE2 = D3V3AEN ? D3V3A_BYTE2_O : 8'bzzzzzzzz;
-assign D3V3A_BYTE3 = D3V3AEN ? D3V3A_BYTE3_O : 8'bzzzzzzzz;
+assign DA_BYTE0 = DAEN ? DA_BYTE0_O : 8'bzzzzzzzz;
+assign DA_BYTE1 = DAEN ? DA_BYTE1_O : 8'bzzzzzzzz;
+assign DA_BYTE2 = DAEN ? DA_BYTE2_O : 8'bzzzzzzzz;
+assign DA_BYTE3 = DAEN ? DA_BYTE3_O : 8'bzzzzzzzz;
 
-wire D3V3BEN = !RnW && !nBG && nRESET;
+wire DBEN = !RnW && !nBG && nRESET;
 
-assign D3V3B_BYTE0 = D3V3BEN ? D3V3B_BYTE0_O : 8'bzzzzzzzz;
-assign D3V3B_BYTE1 = D3V3BEN ? D3V3B_BYTE1_O : 8'bzzzzzzzz;
-assign D3V3B_BYTE2 = D3V3BEN ? D3V3B_BYTE2_O : 8'bzzzzzzzz;
-assign D3V3B_BYTE3 = D3V3BEN ? D3V3B_BYTE3_O : 8'bzzzzzzzz;
+assign DB_BYTE0 = DBEN ? DB_BYTE0_O : 8'bzzzzzzzz;
+assign DB_BYTE1 = DBEN ? DB_BYTE1_O : 8'bzzzzzzzz;
+assign DB_BYTE2 = DBEN ? DB_BYTE2_O : 8'bzzzzzzzz;
+assign DB_BYTE3 = DBEN ? DB_BYTE3_O : 8'bzzzzzzzz;
 
 //////////////////////////
 // DEFINE THE WRITE I/O //
@@ -177,67 +179,65 @@ assign D3V3B_BYTE3 = D3V3BEN ? D3V3B_BYTE3_O : 8'bzzzzzzzz;
 //THE CYCLE SIZE DESCRIPTORS (SIZ0, SIZ1, A0, A1) ARE USED BY THE TARGET DEVICE TO
 //IDENTIFY THE CORRECT BYTES FOR LATCHING.
 
-assign D3V3B_BYTE0_O =
-    STATE == 4'b0001 ? D3V3A_BYTE0 :
-    STATE == 4'b0010 ? D3V3A_BYTE2 :
-    STATE == 4'b0011 ? D3V3A_BYTE0 :
-    STATE == 4'b0100 ? D3V3A_BYTE2 :
-    STATE == 4'b0101 ? D3V3A_BYTE0 :
-    STATE == 4'b0110 ? D3V3A_BYTE1 :
-    STATE == 4'b0111 ? D3V3A_BYTE2 :
-    STATE == 4'b1000 ? D3V3A_BYTE3 :
+assign DB_BYTE0_O =
+    STATE == 4'b0001 ? DA_BYTE0 :
+    STATE == 4'b0010 ? DA_BYTE2 :
+    STATE == 4'b0011 ? DA_BYTE0 :
+    STATE == 4'b0100 ? DA_BYTE2 :
+    STATE == 4'b0101 ? DA_BYTE0 :
+    STATE == 4'b0110 ? DA_BYTE1 :
+    STATE == 4'b0111 ? DA_BYTE2 :
+    STATE == 4'b1000 ? DA_BYTE3 :
     8'b00000000;
     
-assign D3V3B_BYTE1_O =
-    STATE == 4'b0001 ? D3V3A_BYTE1 :
-    STATE == 4'b0010 ? D3V3A_BYTE3 :
-    STATE == 4'b0011 ? D3V3A_BYTE1 :
-    STATE == 4'b0100 ? D3V3A_BYTE3 :
-    STATE == 4'b0101 ? D3V3A_BYTE1 :
-    STATE == 4'b0110 ? D3V3A_BYTE1 :
-    STATE == 4'b0111 ? D3V3A_BYTE1 :
-    STATE == 4'b1000 ? D3V3A_BYTE3 :
+assign DB_BYTE1_O =
+    STATE == 4'b0001 ? DA_BYTE1 :
+    STATE == 4'b0010 ? DA_BYTE3 :
+    STATE == 4'b0011 ? DA_BYTE1 :
+    STATE == 4'b0100 ? DA_BYTE3 :
+    STATE == 4'b0101 ? DA_BYTE1 :
+    STATE == 4'b0110 ? DA_BYTE1 :
+    STATE == 4'b0111 ? DA_BYTE1 :
+    STATE == 4'b1000 ? DA_BYTE3 :
     8'b00000000;
 
-assign D3V3B_BYTE2_O =
-    STATE == 4'b0001 ? D3V3A_BYTE2 :
-    STATE == 4'b0010 ? D3V3A_BYTE2 :
-    STATE == 4'b0011 ? D3V3A_BYTE2 :
-    STATE == 4'b0100 ? D3V3A_BYTE2 :
-    STATE == 4'b0101 ? D3V3A_BYTE2 :
-    STATE == 4'b0110 ? D3V3A_BYTE2 :
-    STATE == 4'b0111 ? D3V3A_BYTE2 :
-    STATE == 4'b1000 ? D3V3A_BYTE3 :
+assign DB_BYTE2_O =
+    STATE == 4'b0001 ? DA_BYTE2 :
+    STATE == 4'b0010 ? DA_BYTE2 :
+    STATE == 4'b0011 ? DA_BYTE2 :
+    STATE == 4'b0100 ? DA_BYTE2 :
+    STATE == 4'b0101 ? DA_BYTE2 :
+    STATE == 4'b0110 ? DA_BYTE2 :
+    STATE == 4'b0111 ? DA_BYTE2 :
+    STATE == 4'b1000 ? DA_BYTE3 :
     8'b00000000;
 
-assign D3V3B_BYTE3_O =
-    STATE == 4'b0001 ? D3V3A_BYTE3 :
-    STATE == 4'b0010 ? D3V3A_BYTE3 :
-    STATE == 4'b0011 ? D3V3A_BYTE3 :
-    STATE == 4'b0100 ? D3V3A_BYTE3 :
-    STATE == 4'b0101 ? D3V3A_BYTE3 :
-    STATE == 4'b0110 ? D3V3A_BYTE3 :
-    STATE == 4'b0111 ? D3V3A_BYTE3 :
-    STATE == 4'b1000 ? D3V3A_BYTE3 :
+assign DB_BYTE3_O =
+    STATE == 4'b0001 ? DA_BYTE3 :
+    STATE == 4'b0010 ? DA_BYTE3 :
+    STATE == 4'b0011 ? DA_BYTE3 :
+    STATE == 4'b0100 ? DA_BYTE3 :
+    STATE == 4'b0101 ? DA_BYTE3 :
+    STATE == 4'b0110 ? DA_BYTE3 :
+    STATE == 4'b0111 ? DA_BYTE3 :
+    STATE == 4'b1000 ? DA_BYTE3 :
     8'b00000000;
 
 ///////////////////////////////
 // LATCH DATA ON READ CYCLES //
 ///////////////////////////////
 
-wire LATCH = DSACK != 2'b11;
-
 always @(posedge BCLK, negedge nRESET) begin
     if (!nRESET) begin
 
-        D3V3A_BYTE0_O <= 8'b00000000;
-        D3V3A_BYTE1_O <= 8'b00000000;
-        D3V3A_BYTE2_O <= 8'b00000000;
-        D3V3A_BYTE3_O <= 8'b00000000;
+        DA_BYTE0_O <= 8'b00000000;
+        DA_BYTE1_O <= 8'b00000000;
+        DA_BYTE2_O <= 8'b00000000;
+        DA_BYTE3_O <= 8'b00000000;
 
     end else begin
 
-        if (LATCH) begin
+        if (DSACK != 2'b11) begin
 
             case (STATE)
 
@@ -246,71 +246,71 @@ always @(posedge BCLK, negedge nRESET) begin
                                     
                         2'b00: //32-BIT PORT	
                             begin
-                                D3V3A_BYTE0_O <= D3V3B_BYTE0;
-                                D3V3A_BYTE1_O <= D3V3B_BYTE1;
-                                D3V3A_BYTE2_O <= D3V3B_BYTE2;
-                                D3V3A_BYTE3_O <= D3V3B_BYTE3;
+                                DA_BYTE0_O <= DB_BYTE0;
+                                DA_BYTE1_O <= DB_BYTE1;
+                                DA_BYTE2_O <= DB_BYTE2;
+                                DA_BYTE3_O <= DB_BYTE3;
                             end
                             
                         2'b01: //16-BIT PORT
                             begin
-                                D3V3A_BYTE0_O <= D3V3B_BYTE0;
-                                D3V3A_BYTE1_O <= D3V3B_BYTE1;
+                                DA_BYTE0_O <= DB_BYTE0;
+                                DA_BYTE1_O <= DB_BYTE1;
                             end
                     endcase
 
                 4'b0010: //16-BIT PORT, COMPLETE THE LONG WORD TRANSFER B
                     begin
-                        D3V3A_BYTE2_O <= D3V3B_BYTE0;
-                        D3V3A_BYTE3_O <= D3V3B_BYTE1;
+                        DA_BYTE2_O <= DB_BYTE0;
+                        DA_BYTE3_O <= DB_BYTE1;
                     end
 
                 4'b0011: //WORD TRANSFER, ADDRESS 0 F,3
                     begin
-                        D3V3A_BYTE0_O <= D3V3B_BYTE0;
-                        D3V3A_BYTE1_O <= D3V3B_BYTE1;
-                        D3V3A_BYTE2_O <= D3V3B_BYTE2;
-                        D3V3A_BYTE3_O <= D3V3B_BYTE3;
+                        DA_BYTE0_O <= DB_BYTE0;
+                        DA_BYTE1_O <= DB_BYTE1;
+                        DA_BYTE2_O <= DB_BYTE2;
+                        DA_BYTE3_O <= DB_BYTE3;
                     end
 
                 4'b0100: //WORD TRANSFER, ADDRESS 2 G,4
                     begin
-                        D3V3A_BYTE0_O <= D3V3B_BYTE2;
-                        D3V3A_BYTE1_O <= D3V3B_BYTE3;
-                        D3V3A_BYTE2_O <= D3V3B_BYTE2;
-                        D3V3A_BYTE3_O <= D3V3B_BYTE3;
+                        DA_BYTE0_O <= DB_BYTE2;
+                        DA_BYTE1_O <= DB_BYTE3;
+                        DA_BYTE2_O <= DB_BYTE2;
+                        DA_BYTE3_O <= DB_BYTE3;
                     end
 
                 4'b0101: //BYTE TRANSFER, ADDRESS 0 K,5
                     begin
-                        D3V3A_BYTE0_O <= D3V3B_BYTE0;
-                        D3V3A_BYTE1_O <= D3V3B_BYTE1;
-                        D3V3A_BYTE2_O <= D3V3B_BYTE2;
-                        D3V3A_BYTE3_O <= D3V3B_BYTE3;
+                        DA_BYTE0_O <= DB_BYTE0;
+                        DA_BYTE1_O <= DB_BYTE1;
+                        DA_BYTE2_O <= DB_BYTE2;
+                        DA_BYTE3_O <= DB_BYTE3;
                     end
 
                 4'b0110: //BYTE TRANSFER, ADDRESS 1 L,6
                     begin
-                        D3V3A_BYTE0_O <= D3V3B_BYTE1;
-                        D3V3A_BYTE1_O <= D3V3B_BYTE1;
-                        D3V3A_BYTE2_O <= D3V3B_BYTE2;
-                        D3V3A_BYTE3_O <= D3V3B_BYTE3;
+                        DA_BYTE0_O <= DB_BYTE1;
+                        DA_BYTE1_O <= DB_BYTE1;
+                        DA_BYTE2_O <= DB_BYTE2;
+                        DA_BYTE3_O <= DB_BYTE3;
                     end
 
                 4'b0111: //BYTE TRANSFER, ADDRESS 2 M,7
                     begin
-                        D3V3A_BYTE0_O <= D3V3B_BYTE2;
-                        D3V3A_BYTE1_O <= D3V3B_BYTE1;
-                        D3V3A_BYTE2_O <= D3V3B_BYTE2;
-                        D3V3A_BYTE3_O <= D3V3B_BYTE3;
+                        DA_BYTE0_O <= DB_BYTE2;
+                        DA_BYTE1_O <= DB_BYTE1;
+                        DA_BYTE2_O <= DB_BYTE2;
+                        DA_BYTE3_O <= DB_BYTE3;
                     end
 
                 4'b1000: //BYTE TRANSFER, ADDRESS 3 N,8
                     begin
-                        D3V3A_BYTE0_O <= D3V3B_BYTE3;
-                        D3V3A_BYTE1_O <= D3V3B_BYTE3;
-                        D3V3A_BYTE2_O <= D3V3B_BYTE2;
-                        D3V3A_BYTE3_O <= D3V3B_BYTE3;
+                        DA_BYTE0_O <= DB_BYTE3;
+                        DA_BYTE1_O <= DB_BYTE3;
+                        DA_BYTE2_O <= DB_BYTE2;
+                        DA_BYTE3_O <= DB_BYTE3;
                     end
 
             endcase
@@ -421,14 +421,12 @@ always @(posedge BCLK, negedge nRESET) begin
                                     STATE <= 4'b0010; 
                                     TS <= 0; 
                                     CYCLE1 <= 0;
-                                end else begin
-                                    if (TA) begin
-                                        TA <= 0; 
-                                        TBI <= 0;
-                                        TCI <= 0;
-                                        if (!BURST || (CYCLE_BURST_INHIBIT && BURST_COUNTER == 1'b1) || BURST_COUNTER == 3'b100) begin                                                                        
-                                            STATE <= 4'b0000; 
-                                        end
+                                end else if (TA) begin
+                                    TA <= 0; 
+                                    TBI <= 0;
+                                    TCI <= 0;
+                                    if (!BURST || (CYCLE_BURST_INHIBIT && BURST_COUNTER == 1'b1) || BURST_COUNTER == 3'b100) begin                                                                        
+                                        STATE <= 4'b0000; 
                                     end
                                 end
                             end
