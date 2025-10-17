73a9050da8c80d2e5d5cb594d336fdb81199934e
Jason Neus <jason50146@yahoo.com>
2025-10-16 20:56:34 -0500

    U110 "slow" - Implement same PIO mode selection as U110 "fast".

--- Verilog/Daily/AmigaPCI/U110_SLOW/U110_ATA.v
+++ Verilog/Daily/AmigaPCI/U110_SLOW/U110_ATA.v
@@ -22,11 +22,10 @@ Target Devices: iCE40-HX4K-TQ144
 
 Description: ATA Controller
 
-Revision History:
 Date          Who  Description
 -----------------------------------
 02-JUL-2025   JN   Initial release for Rev 6.0 hardware.
-12-OCT-2025   JN   Move negation of DOIR/DIOW back one clock.
+16-OCT-2025   JN   Added user selectable ATA timing.
 
 GitHub: https://github.com/jasonsbeer/AmigaPCI
 */
@@ -34,17 +33,19 @@ GitHub: https://github.com/jasonsbeer/AmigaPCI
 module U110_ATA (
 
     //Clocks
-    input CLK40, 
+    input CLK40,
     
-    //Cycle Start/Termination
+    //Cycle Start/Terminate
     input RESETn, TSn, RnW,
     output reg ATA_TACK, ATA_LATCH,
 
-    //ATA Address Signals
+    //ATA Addresses
     input ATA_ENn, PPIO, SPIO, PCS1 , PCS0, SCS1, SCS0,
+
+    //ATA Chip Selects
     output CS0_PRIn, CS1_PRIn, CS0_SECn, CS1_SECn,
     output DIOR_PRIn, DIOW_PRIn, DIOR_SECn, DIOW_SECn
-
+    
 );
 
   ///////////////////////////
@@ -52,32 +53,46 @@ module U110_ATA (
 ///////////////////////////
 
 //T1 IS THE TIME AFTER ADDRESS IS VALID THAT WE ASSERT _DIOR/_DIOW
-//T2 IS THE TIME AFTER ASSERTION OF _DIOR/_DIOW THAT READ DATA IS VALID OR WRITE DATA IS LATCHED.
+//T2 IS THE TIME AFTER ASSERTION OF _DIOR/_DIOW THAT READ DATA IS VALID OR WRITE DATA CAN BE LATCHED.
 //T0 IS THE TOTAL CYCLE TIME.
-//THESE TIMES ARE CUMULATIVE. THEY ARE THE TOTAL CLOCKS TO THAT POINT.
+//THESE TIMES ARE CUMULATIVE. THEY ARE THE TOTAL CLOCKS TO THAT POINT STARTING AT BASE ZERO.
 
 //THESE ARE THE RULES FOR THE FINITE STATE MACHINE TO WORK.
 //   NO TWO VALUES CAN BE THE SAME.
-//   T1 MUST BE AT LEAST 1.
+//   T1 MAY BE ZERO.
 //   T2 MUST BE AT LEAST 2 GREATER THAN T1.
 
-//WE SUBTRACT ONE CLOCK FROM T1 BECAUSE THE ADDRESS IS VALID ONE CLOCK BEFORE _TS ASSERTS.
+//SUBTRACT ONE CLOCK FROM T1 BECAUSE THE ADDRESS IS VALID ONE CLOCK (25ns) BEFORE _TS ASSERTS.
 
-localparam M0_T1 = 8'd2;  //3
-localparam M0_T2 = 8'd9;  //7
-localparam M0_T0 = 8'd24; //24
+//For read cycles, We wait for T2 to be met and then assert _TACK on the next clock. _DIOR is then negated the clock after that.
+//For write cycles, we negate _DIOW at T2 and then assert _TACK one clock after that.
 
-localparam M2_T1 = 8'd1; //2
-localparam M2_T2 = 8'd5; //4
-localparam M2_T0 = 8'd9; //10
+//localparam M0_T1 = 8'd2;  //70ns
+//localparam M0_T2 = 8'd12; //290ns
+//localparam M0_T0 = 8'd24; //600ns
 
-localparam M4_T1 = 8'd1; //1
-localparam M4_T2 = 8'd4; //3
-localparam M4_T0 = 8'd5; //5
+localparam M2_T1 = 4'd1; //30ns
+localparam M2_T2 = 4'd5; //100ns
+localparam M2_T0 = 4'd9; //240ns
 
-wire [3:0] T1 = M4_T1;
-wire [3:0] T2 = M4_T2;
-wire [3:0] T0 = M4_T0;
+localparam M4_T1 = 4'd0; //25ns
+localparam M4_T2 = 4'd3; //70ns
+localparam M4_T0 = 4'd5; //120ns
+
+wire CS_PRIMARY   = (!CS0_PRIn || !CS1_PRIn);
+wire CS_SECONDARY = (!CS0_SECn || !CS1_SECn);
+
+wire [3:0] P_T1 = PPIO ? M4_T1 : M2_T1;
+wire [3:0] S_T1 = SPIO ? M4_T1 : M2_T1;
+
+wire [3:0] T1_ACTIVE = P_CYCLE ? (PPIO ? M4_T1 : M2_T1)
+                               : (SPIO ? M4_T1 : M2_T1);
+
+wire [3:0] T2_ACTIVE = P_CYCLE ? (PPIO ? M4_T2 : M2_T2)
+                               : (SPIO ? M4_T2 : M2_T2);
+
+wire [3:0] T0_ACTIVE = P_CYCLE ? (PPIO ? M4_T0 : M2_T0)
+                               : (SPIO ? M4_T0 : M2_T0);
 
   //////////////////////
  // ATA CHIP SELECTS //
@@ -88,71 +103,67 @@ assign CS1_PRIn = !(!ATA_ENn && PCS1);
 assign CS0_SECn = !(!ATA_ENn && SCS0);
 assign CS1_SECn = !(!ATA_ENn && SCS1);
 
-//Test
-//assign CS0_SECn = !(!ATA_ENn && PCS0);
-//assign CS1_SECn = !(!ATA_ENn && PCS1);
-
-assign DIOR_PRIn = !(RW_EN &&  RnW && (!CS0_PRIn || !CS1_PRIn));
-assign DIOW_PRIn = !(RW_EN && !RnW && (!CS0_PRIn || !CS1_PRIn));
-assign DIOR_SECn = !(RW_EN &&  RnW && (!CS0_SECn || !CS1_SECn));
-assign DIOW_SECn = !(RW_EN && !RnW && (!CS0_SECn || !CS1_SECn));
-
-//Test
-//assign DIOR_SECn = !(RW_EN &&  RnW && (!CS0_PRIn || !CS1_PRIn));
-//assign DIOW_SECn = !(RW_EN && !RnW && (!CS0_PRIn || !CS1_PRIn));
+assign DIOR_PRIn = !(RW_EN && !WRITE_CYCLE && P_CYCLE);
+assign DIOW_PRIn = !(RW_EN &&  WRITE_CYCLE && P_CYCLE);
+assign DIOR_SECn = !(RW_EN && !WRITE_CYCLE && S_CYCLE);
+assign DIOW_SECn = !(RW_EN &&  WRITE_CYCLE && S_CYCLE);
 
   ///////////////////////
  // ATA STATE MACHINE //
 ///////////////////////
 
-reg [7:0] CYCLE_COUNT;
-reg ATA_START;
-reg ATA_CYCLE;
-reg RW_EN;
+reg P_CYCLE, S_CYCLE, RW_EN, WRITE_CYCLE, ATA_PENDING;
+reg [3:0] CYCLE_COUNT;
 
 always @(posedge CLK40) begin
-    if (!RESETn) begin
-        CYCLE_COUNT <= 8'h00;
-        ATA_TACK <= 0;
-        ATA_LATCH <= 1;
-        ATA_START <= 0;
-        ATA_CYCLE <= 0;
-        RW_EN <= 0;
+    if (!RESETn) begin        
+        ATA_TACK    <= 0;
+        WRITE_CYCLE <= 0;
+        RW_EN       <= 0;
+        P_CYCLE     <= 0;
+        S_CYCLE     <= 0;
+        ATA_LATCH   <= 1;
+        ATA_PENDING <= 0;
+        CYCLE_COUNT <= 4'h0;
     end else begin
 
-        ATA_START <= (((!TSn && !ATA_ENn) || ATA_START) && !ATA_CYCLE);
-        CYCLE_COUNT <= CYCLE_COUNT != 8'h00 ? CYCLE_COUNT + 1 : CYCLE_COUNT;
+        if (P_CYCLE || S_CYCLE) begin
 
-        case (CYCLE_COUNT)
+                if (!TSn && !ATA_ENn) begin
+                    ATA_PENDING <= 1;
+                 end
+                 
+                 CYCLE_COUNT <= CYCLE_COUNT + 1;
 
-            8'h00 : begin
-                if ((!TSn && !ATA_ENn) || ATA_START) begin
-                    ATA_CYCLE <= 1;
-                    CYCLE_COUNT <= 8'h01;
+                if (CYCLE_COUNT == T1_ACTIVE) begin
+                    RW_EN <= 1;
+                end else if (CYCLE_COUNT == T2_ACTIVE - 1) begin
+                    ATA_TACK <= 1;
+                end else if (CYCLE_COUNT == T2_ACTIVE) begin
+                    RW_EN <= !(WRITE_CYCLE);
+                    ATA_TACK <= 0;                
+                end else begin
+                    if (CYCLE_COUNT == T0_ACTIVE) begin
+                        P_CYCLE <= 0;
+                        S_CYCLE <= 0;
+                        RW_EN <= 0;
+                    end else if (CYCLE_COUNT == T2_ACTIVE + 2) begin
+                        RW_EN <= 0;
+                    end
                 end
+
+        end else begin
+
+            if ((!TSn && !ATA_ENn) || ATA_PENDING) begin
+                ATA_PENDING <= 0;
+                WRITE_CYCLE <= !RnW;
+                CYCLE_COUNT <= 4'h1;
+                P_CYCLE <= CS_PRIMARY;
+                S_CYCLE <= CS_SECONDARY;
+                RW_EN <= ((CS_PRIMARY && P_T1 == 4'h0) || (CS_SECONDARY && S_T1 == 4'h0));
             end
 
-            T1 : begin
-                ATA_CYCLE <= 0;
-                RW_EN <= 1;
-            end
-
-            T2 - 1 : begin
-                ATA_TACK <= RnW; //Read ACK
-            end
-
-            T2 : begin
-                ATA_TACK <= !RnW; //Write ACK
-                //ATA_LATCH <= !RnW;
-            end
-
-            T0 : begin
-                RW_EN <= 0;
-                CYCLE_COUNT <= 8'h00;
-                ATA_TACK <= 0;
-            end
-
-        endcase
+        end
     end
 end
 
--- Verilog/Daily/AmigaPCI/U110_SLOW/U110_TOP.v
+++ Verilog/Daily/AmigaPCI/U110_SLOW/U110_TOP.v
@@ -27,34 +27,62 @@ See individual modules for revision history.
 GitHub: https://github.com/jasonsbeer/AmigaPCI
 
 iceprog D:\AmigaPCI\U110\APCI_U110\APCI_U110_Implmnt\sbt\outputs\bitmap\U110_TOP_bitmap.bin
+
+NOTE:This one has the "slow" PLL!
 */
 
 module U110_TOP (
 
-    input CLK40_IN, CLK33, RESETn, TSn, RnW,
+    //Clocks
+    input CLK40_IN, CLK33,
+    
+    //Cycle Start/Terminate
+    input RESETn, TSn, RnW,
+    input [1:0] SIZ,
+    output TEAn, TACKn, TCIn, TBIn,
+    
+    //ATA Chip Selects
     input ATA_ENn, PPIO, SPIO, PCS1 , PCS0, SCS1, SCS0,
+    output CS0_PRIn, CS1_PRIn, CS0_SECn, CS1_SECn, DIOR_PRIn, DIOW_PRIn, DIOR_SECn, DIOW_SECn,
+
+    //ATA Buffers
+    output IDELENn, IDEDIR, IDEHRENn, IDEHWENn, ATA_LATCH,
+
+    //PCI 
     input BPRO_ENn, DEVSELn, TRDYn, PHASEA_D,
     input [1:0] PCIAT,
-    input [1:0] SIZ,
+    //output PCI_CYCLEn,
 
-    output INT2n, TEAn, TACKn, TCIn, TBIn, BUSDIR,
-    output IDELENn, IDEDIR, IDEHRENn, IDEHWENn, ATA_LATCH, //PCI_CYCLEn,
-    output BGn, BURSTn,
-    output CS0_PRIn, CS1_PRIn, CS0_SECn, CS1_SECn, DIOR_PRIn, DIOW_PRIn, DIOR_SECn, DIOW_SECn
-
-    //Test
-    //,output TP2
+    //Arbitor and Interrupts
+    output INT2n, BUSDIR, BGn, BURSTn
 
 );
 
-//assign TP2 = CLK40_PLL;
-
-  ////////////////////
- // INTERNAL WIRES //
-////////////////////
-
+wire CLK40_PAD = CLK40_IN;
 wire CLK40 = CLK40_PLL;
-wire CLK40_PAD = !CLK40_IN;
+
+  /////////
+ // PLL //
+/////////
+
+//DOUBLE CHECK THIS WITH ICECUBE2!
+/*wire CLK66;
+
+SB_PLL40_CORE #(
+    .FEEDBACK_PATH("SIMPLE"),
+    .PLLOUT_SELECT("GENCLK"),
+    .DIVR(4'b0000),     // DIVR = 0
+    .DIVF(7'b0000001),  // DIVF = 9
+    .DIVQ(3'b000),      // DIVQ = 1
+    .FILTER_RANGE(3'b001)
+) pll_inst (
+    .REFERENCECLK(CLK33),
+    .PLLOUTCORE(CLK66),
+    //.PLLOUTGLOBAL(CLK66);
+    .LOCK(),
+    .RESETB(1'b1),
+    .BYPASS(1'b0)
+);*/
 
   ///////////////
  // INTERRUPT //
@@ -173,32 +201,26 @@ U110_ARBITOR U110_ARBITOR (
  // PLL //
 /////////
 
-//We are using the PLL here to accomodate some shifting of edge assertions due to latency.
-
-//Effect of FDA_FEEDBACK value
-//0  = ~5ns early peak
-//7  = ~Xns early peak
-//15 = ~Xns early peak
-
 SB_PLL40_CORE #(
     .DIVR (4'b0000),
     .DIVF (7'b0000000),
     .DIVQ (3'b100),
     .FILTER_RANGE (3'b011),
-    .FEEDBACK_PATH ("DELAY"),
+    .FEEDBACK_PATH ("PHASE_AND_DELAY"),
     .DELAY_ADJUSTMENT_MODE_FEEDBACK ("FIXED"),
-    .FDA_FEEDBACK   (4'b0000),
-    .DELAY_ADJUSTMENT_MODE_RELATIVE ("FIXED"),
-    .FDA_RELATIVE   (4'b0000),
-    .PLLOUT_SELECT ("GENCLK")
+    .FDA_FEEDBACK   (4'b1111),
+    //.DELAY_ADJUSTMENT_MODE_RELATIVE ("FIXED"),
+    //.FDA_RELATIVE   (4'b0000),
+    .PLLOUT_SELECT ("SHIFTREG_0deg"),
+    .SHIFTREG_DIV_MODE (1'b0)
 ) pll (
     .LOCK           (),
     .RESETB         (1'b1),
     .REFERENCECLK   (CLK40_PAD),
     .PLLOUTGLOBAL   (CLK40_PLL),
-
+    
     .EXTFEEDBACK       (1'b0),
-    .DYNAMICDELAY      (8'b00000000),
+    .DYNAMICDELAY      (8'b00001111),
     .BYPASS            (1'b0),
     .SDI               (1'b0),
     .SCLK              (1'b0),
