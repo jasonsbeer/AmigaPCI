8f3cd575e71626281debfa35d927bc9ef9bf077d
Jason Neus <jason50146@yahoo.com>
2025-11-06 19:40:57 -0600

    U409 Daily 06-NOV-2025
    Fixed RTC address range.

---
+++ Verilog/Daily/AmigaPCI/U409/U409_AUTOCONFIG.v
@@ -0,0 +1,259 @@
+/*
+LICENSE:
+
+This work is released under the Creative Commons Attribution-NonCommercial 4.0 International
+https://creativecommons.org/licenses/by-nc/4.0/
+
+You are free to:
+Share — copy and redistribute the material in any medium or format
+Adapt — remix, transform, and build upon the material
+The licensor cannot revoke these freedoms as long as you follow the license terms.
+
+Under the following terms:
+Attribution — You must give appropriate credit , provide a link to the license, and indicate if changes were made . You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
+NonCommercial — You may not use the material for commercial purposes .
+No additional restrictions — You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.
+
+RTL MODULE:
+
+Engineer: Jason Neus
+Design Name: U409
+Module Name: U409_AUTOCONFIG
+Project Name: AmigaPCI
+Target Devices: iCE40-HX4K-TQ144
+
+Description: AUTOCONFIG
+
+Date          Who  Description
+-----------------------------------
+01-JUL-2025   JN   INITIAL REV 6.0 CODE
+11-OCT-2025   JN   Flipped SM to positive clock edge.
+14-OCT-2025   JN   Changed prometheus device to 256MB.
+
+GitHub: https://github.com/jasonsbeer/AmigaPCI
+*/
+
+module U409_AUTOCONFIG (
+
+    //Clocks
+    input CLK40, RESETn,
+    
+    //Cycle Start/Termination
+    input AUTOCONFIG_SPACE, RnW, TSn,
+    output reg AC_TACK,
+
+    //Data and Address Bus
+    input [3:0] D_IN, //D31-28
+    input [7:1] A,
+    output [3:0] D_OUT, //D31-28
+
+    //Configuration Signals
+    input  AUTOBOOT, //CPUCONFn, 
+    output reg CONFIGENn, CONFIGURED,
+
+    //Base Addresses
+    output reg [7:0] BRIDGE_BASE,
+    output reg [7:1] LIDE_BASE,
+    output reg [3:0] PRO_BASE
+    
+);
+
+  ////////////////
+ // PARAMETERS //
+////////////////
+
+localparam BRIDGE_PID = 8'd4;
+localparam LIDE_PID = 8'd3;
+localparam MNF = 16'd600;
+localparam FS_PID = 8'd200;
+localparam FS_MNF = 16'd3643;
+localparam SERNUM = 32'd1;
+
+  //////////////
+ // DATA BUS //
+//////////////
+
+assign D_OUT = !BRIDGE_CONF ? BRIDGE_OUT :
+               !LIDE_CONF   ? LIDE_OUT :
+               !CONFIGURED  ? PR_OUT :
+                              4'hF;
+
+  /////////////////
+ // ADDRESS BUS //
+/////////////////
+
+wire [7:0] AC_AD = {A[7:1], 1'b0};
+
+  ////////////////
+ // AUTOCONFIG //
+////////////////
+
+//The LIDE ATA device is configured as a 128k device with optional autoboot, as defined by user jumper J900.
+//The PCI bridge is actually configured twice. Once as a 64k device for "Made for 68K" devices and once as 
+//a 258MB Prometheum compatable device.
+
+reg BRIDGE_CONF;
+reg LIDE_CONF;
+reg [3:0] BRIDGE_OUT;
+reg [3:0] LIDE_OUT;
+reg [3:0] PR_OUT;
+reg [3:0] STATE;
+
+always @(posedge CLK40) begin
+    if (!RESETn) begin
+        LIDE_CONF <= 0;
+        LIDE_OUT <= 4'h0;
+        LIDE_BASE <= 7'b0;
+
+        BRIDGE_CONF <= 0;
+        BRIDGE_BASE <= 4'h0;
+        BRIDGE_OUT <= 4'h0;
+
+        CONFIGURED <= 0;
+        PRO_BASE <= 3'b0;
+        PR_OUT <= 4'h0;
+
+        AC_TACK <= 0;
+        CONFIGENn <= 1;
+        STATE <= 4'h0;
+    end else begin
+        case (STATE)
+            4'h0 : begin
+                AC_TACK <= 0;
+                if (!CONFIGURED && AUTOCONFIG_SPACE && !TSn) begin
+                    STATE <= 4'h01;
+                    if (RnW) begin
+                        case (AC_AD)
+                            8'h00 : begin
+                                BRIDGE_OUT <= 4'b1100;
+                                LIDE_OUT   <= {4'b110, AUTOBOOT};
+                                PR_OUT     <= 4'b1000;
+                            end
+                            8'h02 : begin
+                                BRIDGE_OUT <= 4'b0001; //64K address space.
+                                LIDE_OUT   <= 4'b0010; //128K address space.
+                                PR_OUT     <= 4'b0100; //256MB address space.
+                            end
+                            8'h04 : begin //Product Number High Nibble
+                                BRIDGE_OUT <= ~(BRIDGE_PID[7:4]);
+                                LIDE_OUT   <= ~(LIDE_PID[7:4]);
+                                PR_OUT     <= ~(FS_PID[7:4]);
+                            end
+                            8'h06 : begin //Product Number Low Nibble
+                                BRIDGE_OUT <= ~(BRIDGE_PID[3:0]);
+                                LIDE_OUT   <= ~(LIDE_PID[3:0]);
+                                PR_OUT     <= ~(FS_PID[3:0]);
+                            end
+                            8'h08 : begin
+                                BRIDGE_OUT <= ~(4'b1100);
+                                LIDE_OUT   <= ~(4'b0100);
+                                PR_OUT     <= ~(4'b0111);
+                            end
+                            8'h10 : begin //Manufacturer Number
+                                BRIDGE_OUT <= ~(MNF[15:12]);
+                                LIDE_OUT   <= ~(MNF[15:12]);
+                                PR_OUT     <= ~(FS_MNF[15:12]);
+                            end
+                            8'h12 : begin
+                                BRIDGE_OUT <= ~(MNF[11:8]);
+                                LIDE_OUT   <= ~(MNF[11:8]);
+                                PR_OUT     <= ~(FS_MNF[11:8]);
+                            end //Manufacturer Number
+                            8'h14 : begin
+                                BRIDGE_OUT <= ~(MNF[7:4]);
+                                LIDE_OUT   <= ~(MNF[7:4]);
+                                PR_OUT     <= ~(FS_MNF[7:4]);
+                            end //Manufacturer Number
+                            8'h16 : begin
+                                BRIDGE_OUT <= ~(MNF[3:0]);
+                                LIDE_OUT   <= ~(MNF[3:0]);
+                                PR_OUT     <= ~(FS_MNF[3:0]);
+                            end //Manufacturer Number
+                            8'h18 : begin
+                                BRIDGE_OUT <= ~(SERNUM[31:28]);
+                                LIDE_OUT   <= ~(SERNUM[31:28]);
+                                PR_OUT     <= ~(SERNUM[31:28]);
+                            end
+                            8'h1A : begin //Serial number
+                                BRIDGE_OUT <= ~(SERNUM[27:24]);
+                                LIDE_OUT   <= ~(SERNUM[27:24]);
+                                PR_OUT     <= ~(SERNUM[27:24]);
+                            end
+                            8'h1C : begin //Serial number
+                                BRIDGE_OUT <= ~(SERNUM[23:20]);
+                                LIDE_OUT   <= ~(SERNUM[23:20]);
+                                PR_OUT     <= ~(SERNUM[23:20]);
+                            end
+                            8'h1E : begin //Serial number
+                                BRIDGE_OUT <= ~(SERNUM[19:16]);
+                                LIDE_OUT   <= ~(SERNUM[19:16]);
+                                PR_OUT     <= ~(SERNUM[19:16]);
+                            end
+                            8'h20 : begin //Serial number
+                                BRIDGE_OUT <= ~(SERNUM[15:12]);
+                                LIDE_OUT   <= ~(SERNUM[15:12]);
+                                PR_OUT     <= ~(SERNUM[15:12]);
+                            end
+                            8'h22 : begin //Serial number
+                                BRIDGE_OUT <= ~(SERNUM[11:8]);
+                                LIDE_OUT   <= ~(SERNUM[11:8]);
+                                PR_OUT     <= ~(SERNUM[11:8]);
+                            end
+                            8'h24 : begin //Serial number
+                                BRIDGE_OUT <= ~(SERNUM[7:4]);
+                                LIDE_OUT   <= ~(SERNUM[7:4]);
+                                PR_OUT     <= ~(SERNUM[7:4]);
+                            end
+                            8'h26 : begin //Serial number
+                                BRIDGE_OUT <= ~(SERNUM[3:0]);
+                                LIDE_OUT   <= ~(SERNUM[3:0]);
+                                PR_OUT     <= ~(SERNUM[3:0]);
+                            end
+                            default : begin
+                                BRIDGE_OUT <= ~(4'b0000);
+                                LIDE_OUT   <= ~(4'b0000);
+                                PR_OUT     <= ~(4'b0000);
+                            end
+                        endcase
+                    end
+                end
+            end
+            4'h1 : begin
+                if (RnW) begin
+                    AC_TACK <= 1;
+                    STATE <= 4'h0;
+                end else begin
+                    STATE <= 4'h2;
+                end
+            end
+            4'h2 : begin
+                STATE <= 4'h3;
+                if (AC_AD == 8'h4A) begin
+                    if (!BRIDGE_CONF) begin
+                        BRIDGE_BASE[3:0] <= D_IN;
+                    end else if (!LIDE_CONF) begin
+                        LIDE_BASE[3:1] <= D_IN[3:1];
+                    end
+                end else if (AC_AD == 8'h48) begin
+                    if (!BRIDGE_CONF) begin
+                        BRIDGE_CONF <= 1;
+                        BRIDGE_BASE[7:4] <= D_IN;
+                    end else if (!LIDE_CONF) begin
+                        LIDE_CONF <= 1;
+                        LIDE_BASE[7:4] <= D_IN;
+                    end else begin
+                        PRO_BASE <= D_IN[3:0];
+                        CONFIGENn <= 0;
+                        CONFIGURED <= 1;
+                    end
+                end
+            end
+            4'h3 : begin
+                AC_TACK <= 1;
+                STATE <= 4'h0;
+            end
+        endcase
+    end
+end
+
+endmodule
---
+++ Verilog/Daily/AmigaPCI/U409/U409_TOP.v
@@ -0,0 +1,323 @@
+/*
+LICENSE:
+
+This work is released under the Creative Commons Attribution-NonCommercial 4.0 International
+https://creativecommons.org/licenses/by-nc/4.0/
+
+You are free to:
+Share — copy and redistribute the material in any medium or format
+Adapt — remix, transform, and build upon the material
+The licensor cannot revoke these freedoms as long as you follow the license terms.
+
+Under the following terms:
+Attribution — You must give appropriate credit , provide a link to the license, and indicate if changes were made . You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
+NonCommercial — You may not use the material for commercial purposes .
+No additional restrictions — You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.
+
+RTL MODULE:
+
+Engineer: Jason Neus
+Design Name: U409
+Module Name: U409_TOP
+Project Name: AmigaPCI
+Target Devices: iCE40-HX4K-TQ144
+
+Description: ADDRESS DECODE, ROM, TRANSFER ACK, AUTOCONFIG
+
+See individual modules for revision history.
+
+GitHub: https://github.com/jasonsbeer/AmigaPCI
+
+iceprog D:\AmigaPCI\U409\U409_icecube\U409_icecube_Implmnt\sbt\outputs\bitmap\U409_TOP_bitmap.bin
+*/
+
+module U409_TOP (
+
+    //Clocks
+    input  CLK40_IN, CLK28_IN, XCLK, XCLK_ENn, RESETn,
+    output AGNUS_CLK, TICK60, TICK50, CLK_CIA, 
+    
+    //Cycle Start/Termination    
+    input  TSn, OVL, RnW, //CPUCONFn,
+    output TBIn, TCIn,
+    input [1:0] TT,
+    input [2:0] TM,
+    inout TACKn,
+
+    //Data and Address Bus    
+    input [31:1] A,
+    inout [7:4] D,
+
+    //Chip Selects/Address Spaces
+    output ROM_ENn, BUFENn, CIACS0n, CIACS1n, RAMSPACEn, REGSPACEn, RTC_ENn, PORTSIZE,
+    
+    //Configuration Signals
+    input AUTOBOOT,
+    input [1:0] ROM_DELAY,    
+    output CONFIGENn, 
+
+    //PCI
+    output  BREG_ENn, BPRO_ENn,
+    output [1:0] PCIAT,
+
+    //ATA
+    input PPIO_J, SPIO_J,
+    output PCS0, PCS1, SCS0, SCS1, PPIO, SPIO, ATA_ENn,
+
+    //Flash
+    input  FLASH_RDY,
+    output FLASH_ENn, FLASH_READn, FLASH_WRITEn, FLASH_WPn, FLASH_RSTn,
+    output [1:0] FLASH_BANK
+
+);
+
+//////////////////
+// AGNUS CLOCK //
+////////////////
+
+//Agnus is clocked by the oscillator at X3 unless XCLK is enabled by a video device.
+//In that case, pass the XCLK signal instead.
+
+assign AGNUS_CLK = XCLK_ENn ? CLK28_IN : XCLK;
+
+///////////////////
+// SIGNAL WIRES //
+/////////////////
+
+wire CLK40;
+wire AUTOCONFIG_SPACE;
+wire AUTOVECTOR;
+wire CIA_SPACE;
+wire CIA_ENABLE;
+wire FLASH_TACK;
+wire FLASH_SPACE;
+wire RTC_TACK;
+wire RTC_SPACE;
+wire ROM_SPACE;
+wire CONFIGURED;
+
+wire [7:0] BRIDGE_BASE;
+wire [7:1] LIDE_BASE;
+wire [3:0] PRO_BASE;
+wire [3:0] D_OUT;
+wire [3:0] D_IN = AUTOCONFIG_SPACE && !RnW ? D[7:4] : 4'h0;
+
+wire AGNUS_SPACE = !RAMSPACEn || !REGSPACEn;
+wire LV_SPACE = AGNUS_SPACE || AUTOCONFIG_SPACE || ATA_SPACE || FLASH_SPACE; // || !BRIDGE_ENn; //Enable the main level shifting buffers when we are in the LVTTL space.
+assign PORTSIZE = CIA_SPACE || !REGSPACEn || RTC_SPACE || AUTOCONFIG_SPACE || ATA_SPACE || FLASH_SPACE;
+assign D = AUTOCONFIG_SPACE && RnW ? D_OUT : 4'bz;
+
+///////////////////////
+// TRANSFER ACK TOP //
+/////////////////////
+
+U409_TRANSFER_ACK U409_TRANSFER_ACK (
+    //INPUTS
+    .CLK40_IN (CLK40_IN),
+    .CLK40 (CLK40),
+    .RESETn (RESETn),
+    .TSn (TSn),
+    .ROM_SPACE (ROM_SPACE),
+    .CIA_ENABLE (CIA_ENABLE),
+    .CLK_CIA (CLK_CIA),
+    .AGNUS_SPACE (AGNUS_SPACE),
+    .AUTOVECTOR (AUTOVECTOR),
+    .AC_TACK (AC_TACK),
+    .ROM_DELAY (ROM_DELAY),
+    .FLASH_TACK (FLASH_TACK),
+    .RTC_TACK (RTC_TACK),
+
+    //OUTPUTS
+    .TBIn (TBIn),
+    .TCIn (TCIn),
+    .ROM_ENn (ROM_ENn),
+
+    //INOUTS
+    .TACKn (TACKn)
+);
+
+/////////////////////////////
+// DATA BUFFER ENABLE TOP //
+///////////////////////////
+
+U409_DATA_BUFFERS U409_DATA_BUFFERS (
+    //INPUTS
+    .LV_SPACE (LV_SPACE),
+
+    //OUTPUTS
+    .BUFENn (BUFENn)
+);
+
+/////////////////////////
+// ADDRESS DECODE TOP //
+///////////////////////
+
+U409_ADDRESS_DECODE U409_ADDRESS_DECODE (
+    //INPUTS
+    .CLK40 (CLK40),
+    .RESETn (RESETn),
+    .RnW (RnW),
+    .OVL (OVL),
+    .CIA_ENABLE (CIA_ENABLE),
+    .CONFIGURED (CONFIGURED),
+    .TT (TT),
+    .TM (TM),
+    .A (A[31:12]),
+    .BRIDGE_BASE (BRIDGE_BASE),
+    .LIDE_BASE (LIDE_BASE),
+    .PRO_BASE (PRO_BASE),
+
+    //OUTPUTS
+    .ROM_SPACE (ROM_SPACE),
+    .CIA_SPACE (CIA_SPACE),
+    .CIACS0n (CIACS0n),
+    .CIACS1n (CIACS1n),
+    .RAMSPACEn (RAMSPACEn),
+    .REGSPACEn (REGSPACEn),
+    .AUTOVECTOR (AUTOVECTOR),
+    .RTC_SPACE (RTC_SPACE),
+    .AUTOCONFIG_SPACE (AUTOCONFIG_SPACE),
+    .ATA_SPACE (ATA_SPACE),
+    .ATA_ENn (ATA_ENn),
+    .PCS0 (PCS0),
+    .PCS1 (PCS1),
+    .SCS0 (SCS0),
+    .SCS1 (SCS1),
+    .PCIAT (PCIAT),
+    .BREG_ENn (BREG_ENn),
+    .BPRO_ENn (BPRO_ENn),
+    .FLASH_BANK (FLASH_BANK),
+    .FLASH_SPACE (FLASH_SPACE)
+);
+
+/////////////////////
+// TICK CLOCK TOP //
+///////////////////
+
+U409_TICK U409_TICK (
+    //Inputs
+    .CLK28_IN (CLK28_IN),
+
+    //Outputs
+    .TICK60 (TICK60),
+    .TICK50 (TICK50)
+);
+
+////////////////////
+// CIA CLOCK TOP //
+//////////////////
+
+U409_CIA U409_CIA (
+    .CLK28_IN (CLK28_IN),
+    .RESETn (RESETn),
+    .CIA_SPACE (CIA_SPACE),
+    .CLK_CIA (CLK_CIA),
+    .CIA_ENABLE (CIA_ENABLE)
+);
+
+/////////////////
+// AUTOCONFIG //
+///////////////
+
+U409_AUTOCONFIG U409_AUTOCONFIG (
+    //INPUT
+    .CLK40,
+    .RESETn,
+    .AUTOCONFIG_SPACE (AUTOCONFIG_SPACE),
+    .RnW (RnW),
+    .TSn (TSn),
+    .AUTOBOOT (AUTOBOOT),
+    .D_IN (D_IN),
+    .A (A[7:1]),
+
+    //OUTPUT
+    .BRIDGE_BASE (BRIDGE_BASE),
+    .LIDE_BASE (LIDE_BASE),
+    .PRO_BASE (PRO_BASE),
+    .D_OUT (D_OUT),
+    .CONFIGENn (CONFIGENn),
+    .CONFIGURED (CONFIGURED),
+    .AC_TACK (AC_TACK)
+);
+
+////////////
+// FLASH //
+//////////
+
+U409_FLASH U409_FLASH (
+    //INPUT
+    .CLK40 (CLK40),
+    .RESETn (RESETn),
+    .TSn (TSn),
+    .RnW (RnW),
+    .FLASH_SPACE (FLASH_SPACE),
+    .FLASH_RDY (FLASH_RDY),
+    .A (A[23:1]),
+
+    //OUTPUTS
+    .FLASH_ENn (FLASH_ENn),
+    .FLASH_WPn (FLASH_WPn),
+    .FLASH_READn (FLASH_READn),
+    .FLASH_WRITEn (FLASH_WRITEn),
+    .FLASH_RSTn (FLASH_RSTn),
+    .FLASH_TACK (FLASH_TACK)
+);
+
+//////////
+// RTC //
+////////
+
+U409_RTC_SM U409_RTC_SM (
+    //input
+    .CLK40 (CLK40),
+    .RESETn (RESETn),
+    .TSn (TSn),
+    .RnW (RnW),
+    .RTC_SPACE (RTC_SPACE), 
+
+    //output
+    .RTC_ENn (RTC_ENn),
+    .RTC_TACK (RTC_TACK)
+);
+
+////////////////
+// ATA STUFF //
+//////////////
+
+//Pass through the ATA PIO jumper settings
+assign PPIO = PPIO_J;
+assign SPIO = SPIO_J;
+
+//////////
+// PLL //
+////////
+
+wire CLK40_PAD = CLK40_IN;
+
+SB_PLL40_CORE #(
+    .DIVR (4'b0000),
+    .DIVF (7'b0000000),
+    .DIVQ (3'b100),
+    .FILTER_RANGE (3'b011),
+    .FEEDBACK_PATH ("PHASE_AND_DELAY"),
+    .DELAY_ADJUSTMENT_MODE_FEEDBACK ("FIXED"),
+    .FDA_FEEDBACK   (4'b0000),
+    //.DELAY_ADJUSTMENT_MODE_RELATIVE ("FIXED"),
+    //.FDA_RELATIVE   (4'b0000),
+    .PLLOUT_SELECT ("SHIFTREG_0deg"),
+    .SHIFTREG_DIV_MODE (1'b0)
+) pll (
+    .LOCK           (),
+    .RESETB         (1'b1),
+    .REFERENCECLK   (CLK40_PAD),
+    .PLLOUTGLOBAL   (CLK40),
+    
+    .EXTFEEDBACK       (1'b0),
+    .DYNAMICDELAY      (8'b00001111),
+    .BYPASS            (1'b0),
+    .SDI               (1'b0),
+    .SCLK              (1'b0),
+    .LATCHINPUTVALUE   (1'b0)
+);
+
+endmodule
